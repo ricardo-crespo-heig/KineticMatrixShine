f80cfdd3b7091eae66853f13a29feb99114dbe02
ricqui <ricardo.rodriguescrespo@heig-vd.ch>
2024-12-06 10:03:35 +0100

    Fix negative tables and clean code

--- main/main.ino
+++ main/main.ino
@@ -6,89 +6,13 @@
  Description :
 ******************************************************************/
 
-/*#include <Arduino.h>
-#include "IO.h"
-#include "TESTMOTORS.h"
- 
-// defines pins numbers
-
- 
-void setup() 
-{
-    InitIO();
-}
-
-void loop() 
-{
- 
-    //TestMotors();
-    //CalibDriver();
-    
-    //if(digitalRead(CMDU))
-    //{
-    //    TestMotorsU();
-    //}
-    //else
-    //{
-    //    CalibDriverU();
-    //}
-
-
-}*/
-
 #include <Arduino.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "IO.h"
 #include "TESTMOTORS.h"
 
-/*void setup() {
 
-    InitIO();
-      
-    // Configuration de Timer1 pour une interruption toutes les 1 µs
-    cli();  // Désactive les interruptions globales
-    TCCR1A = 0;
-    TCCR1B = 0;
-    TCNT1 = 0;  // Réinitialise le compteur
-    OCR1A = 15;  // Valeur pour 1 µs (à 16 MHz avec un prédiviseur de 1)
-    TCCR1B |= (1 << WGM12);  // Mode CTC (Clear Timer on Compare Match)
-    TCCR1B |= (1 << CS10);   // Prédivision par 13
-    TIMSK1 |= (1 << OCIE1A);  // Active l'interruption pour Timer1
-
-    // Configuration de Timer2 pour une interruption toutes les 100 ms
-    TCCR2A = 0;
-    TCCR2B = 0;
-    TCNT2 = 0;  // Réinitialise le compteur
-    OCR2A = 249;  // Valeur pour 100 ms avec un prédiviseur de 1024
-    TCCR2A |= (1 << WGM21);  // Mode CTC pour Timer2
-    TCCR2B |= (1 << CS22);// | (1 << CS21) | (1 << CS20);  // Prédivision par 1024
-    TIMSK2 |= (1 << OCIE2A);  // Active l'interruption pour Timer2
-
-    sei();  // Active les interruptions globales
-}
-
-// Interruption pour Timer1 (1 µs)
-ISR(TIMER1_COMPA_vect) 
-{
-    //digitalWrite(STEPB1,!digitalRead(STEPB1));
-    // Code à exécuter toutes les 1 µs
-}
-
-// Interruption pour Timer2 (100 ms)
-ISR(TIMER2_COMPA_vect) 
-{
-    digitalWrite(STEPA1,!digitalRead(STEPA1));
-    // Code à exécuter toutes les 100 ms
-}
-
-void loop() {
-    // Code principal, qui sera exécuté entre les interruptions
-}
-
-*/
-
-//#define NUM_CYCLE     10
 #define NUM_PULSES    6400
 
 #define ROWS          40
@@ -143,8 +67,8 @@ const int tbSteps[ROWS][NBR_MOTORS] =
   {50},
   {50},
   {50},
-  {600},
-  {600},
+  {1000},
+  {1000},
   {50},
   {50},
   {50},
@@ -156,14 +80,14 @@ const int tbSteps[ROWS][NBR_MOTORS] =
   {50},
   {-1000},
   {-1000},
-  {50},
-  {50},
-  {50},
-  {50},  
-  {50},
-  {50},
-  {-600},
-  {-600},
+  {-50},
+  {-50},
+  {-50},
+  {-50},  
+  {-50},
+  {-50},
+  {-1000},
+  {-1000},
   {-50},
   {-50},
   {-50},
@@ -173,58 +97,12 @@ const int tbSteps[ROWS][NBR_MOTORS] =
 };
 
 
-/*const int tbSteps[ROWS][NBR_MOTORS] =
-{
-  {1000},
-  {640},
-  {320},
-  {640},
-  {320},
-  {640},
-  {320},
-  {640},
-  {320},
-  {640}  
-};
-
-const int tbSteps1[ROWS][NBR_MOTORS] =
-{
-  {50},
-  {640},
-  {640},
-  {640},
-  {640},
-  {640},
-  {640},
-  {640},
-  {640},
-  {640}  
-};*/
-
 void setup() {
 
     //Serial.begin(250000); // Initialiser la communication série
     InitIO();
       
     cli();  // Désactive les interruptions globales
-    
-    // Configuration de Timer3 pour une interruption toutes les 100 ms
-    /*TCCR3A = 0;
-    TCCR3B = 0;
-    TCNT3 = 0;  // Réinitialise le compteur
-    OCR3A = 24999;  // Valeur pour 100 ms (à 16 MHz avec un prédiviseur de 64)
-    TCCR3B |= (1 << WGM32);  // Mode CTC (Clear Timer on Compare Match)
-    TCCR3B |= (1 << CS31) | (1 << CS30);  // Prédivision par 64
-    TIMSK3 |= (1 << OCIE3A);  // Active l'interruption pour Timer3*/
-
-    // Configuration de Timer1 pour une interruption toutes les 10 µs
-    /*TCCR1A = 0;
-    TCCR1B = 0;
-    TCNT1 = 0;  // Réinitialise le compteur
-    OCR1A = 159;  // Valeur pour 10 µs (à 16 MHz avec un prédiviseur de 8)
-    TCCR1B |= (1 << WGM12);  // Mode CTC (Clear Timer on Compare Match)
-    TCCR1B |= (1 << CS11);   // Prédivision par 8
-    TIMSK1 |= (1 << OCIE1A);  // Active l'interruption pour Timer1*/
 
     // TIMER 1 for interrupt frequency 100000 Hz:
     TCCR1A = 0; // set entire TCCR1A register to 0
@@ -247,7 +125,7 @@ void setup() {
     digitalWrite(SLEEPA4, HIGH);
 
     pulseCount = tbSteps[0][0]*2;
-    initBresenham(tbSteps[0][0]*2);
+    initBresenham(abs(tbSteps[0][0]*2));
 
     // Optionnel : réinitialiser le buffer
     for (int i = 0; i < BUFFER_SIZE; i++) 
@@ -260,7 +138,7 @@ void setup() {
         calculateBresenhamPoint(buffer);
     }
 
-    if((tbSteps[1][0]*2) >= 0)
+    if((tbSteps[1][0]*2) > 0)
     {
       dirCW = true;
     }
@@ -278,7 +156,7 @@ void setup() {
         PORTD &= ~(1 << DIRA1_BIT); // Mettre à LOW
     }
     
-    initBresenham(tbSteps[1][0]*2);
+    initBresenham(abs(tbSteps[1][0]*2));
 
     for(int i = 0; i < BUFFER_SIZE; i++)
     {
@@ -299,51 +177,6 @@ void setup() {
 ISR(TIMER1_COMPA_vect) 
 {
     static uint16_t countInter = 0;
-
-    
-    /*// Code à exécuter toutes les 50 µs
-    //digitalWrite(STEPA1,!digitalRead(STEPA1));
-    if(pulseCount > 0)
-    {
-        // Alterne l'état du step
-        stepState = !stepState;
-        
-        // Applique cet état sur les broches de step
-        digitalWrite(STEPA1, stepState);
-        digitalWrite(STEPA2, stepState);
-        digitalWrite(STEPA3, stepState);
-        digitalWrite(STEPA4, stepState);
-    }
-    
-    // 2000 * 50 µs = 100 ms
-    if (countInter >= 2000) 
-    {  
-        countInter = 0;       // Réinitialise le compteur
-        flagInter != flagInter;           // Signale que 100 ms sont écoulées
-
-        // Si le nombre de pulses est atteint, change la direction
-        if (cycleCount >= NUM_CYCLE) 
-        {
-            cycleCount = 0;
-    
-            // Inverse la direction
-            direction = !direction;
-            digitalWrite(DIRA1, direction);
-            digitalWrite(DIRA2, direction);
-            digitalWrite(DIRA3, direction);
-            digitalWrite(DIRA4, direction);
-    
-            // Réinitialise le compteur de pulses pour la prochaine rotation
-            cycleCount = 0;
-        }
-        else
-        {
-            cycleCount++;
-            pulseCount = tbSteps[cycleCount][0]*2;
-        }
-    }
-
-    pulseCount--;*/
     
     //digitalWrite(STEPA1, stepState);
     //stepState = !stepState;
@@ -408,6 +241,8 @@ ISR(TIMER1_COMPA_vect)
         //direction = !direction;
         //digitalWrite(DIRA1, direction);
 
+        //Serial.println("Buffer Calculated : " + String(dirCW));
+
         if (dirCW) {
             PORTD |= (1 << DIRA1_BIT);  // Mettre à HIGH
         } else {
@@ -438,28 +273,6 @@ ISR(TIMER1_COMPA_vect)
     {
         countInterCycle = 0;
 
-        /*if(dirCW)
-        {
-          direction = true;
-        }
-        else
-        {
-          direction = false;
-        }
-        //direction = !direction;
-        digitalWrite(DIRA1, direction);*/
-
-        /*if (stepState) {
-            PORTD |= (1 << DIRA1_BIT);  // Mettre à HIGH
-        } else {
-            PORTD &= ~(1 << DIRA1_BIT); // Mettre à LOW
-        }*/
-        
-        /*if (!direction) {
-            readBuffer = buffer1; // Pointe sur le second buffer
-        } else {
-            readBuffer = buffer;  // Pointe sur le premier buffer
-        }*/
     }
 }
 
@@ -494,46 +307,12 @@ bool calculateBresenhamPoint(uint8_t* buffer) {
 void loop() {
     // Code principal, qui sera exécuté entre les interruptions
 
-    /*
-    if(flagInter && oneTime)
-    {
-      oneTime = false;
-      initBresenham(tbSteps[countInterCycle][0]*2);
-    
-      for(int i = 0; i < BUFFER_SIZE; i++)
-      {
-          calculateBresenhamPoint(buffer);
-      }
-      // Envoyer le tableau calculé sur la console
-      Serial.println("Buffer Calculated " + String(countInterCycle) + " : ");
-      for (int i = 0; i < BUFFER_SIZE; i++) {
-          Serial.print(buffer[i]);
-          Serial.print((i % 50 == 49) ? '\n' : ' '); // Sauter une ligne toutes les 50 cases
-      }
-    }
-    else if(oneTime)
-    {
-      oneTime = false;
-      initBresenham(tbSteps[countInterCycle][0]*2);
-
-      for(int i = 0; i < BUFFER_SIZE; i++)
-      {
-          calculateBresenhamPoint(buffer1);
-      }
-      // Envoyer le tableau calculé sur la console
-      Serial.println("Buffer Calculated " + String(countInterCycle) + " : ");
-      for (int i = 0; i < BUFFER_SIZE; i++) {
-          Serial.print(buffer1[i]);
-          Serial.print((i % 50 == 49) ? '\n' : ' '); // Sauter une ligne toutes les 50 cases
-      }
-    }*/
-
     if (oneTime) 
     {
         oneTime = false;
         uint8_t* activeBuffer = flagInter ? buffer : buffer1;
 
-        if((tbSteps[countInterCycle][0] * 2) >= 0)
+        if((tbSteps[countInterCycle][0] * 2) > 0)
         {
           dirCW = true;
         }
@@ -542,7 +321,7 @@ void loop() {
           dirCW = false;
         }
         
-        initBresenham(tbSteps[countInterCycle][0] * 2);
+        initBresenham(abs(tbSteps[countInterCycle][0] * 2));
 
         for (int i = 0; i < BUFFER_SIZE; i++) {
             calculateBresenhamPoint(activeBuffer);
